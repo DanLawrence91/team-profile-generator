4dee14d00af952d6eba0cacb92bb009fbb664b88
Daniel Lawrence <dan.lawrence0810@gmail.com>
2022-02-15 15:39:00 +1030

    commented out class files to write tests

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,1 +1,14 @@
+const { TestWatcher } = require('jest');
 const Employee = require('../lib/Employee');
+
+describe('Employee', () => {
+  describe('Initialization', () => {
+    it('Should create an object with name, id and email if given valid arguments', () => {
+      const employee = new Employee('Dan', 1, 'dan@test.com');
+
+      expect(employee.name).toEqual('Dan');
+      expect(employee.id).toEqual(1);
+      expect(employee.email).toEqual('dan@test.com');
+    });
+  });
+});
