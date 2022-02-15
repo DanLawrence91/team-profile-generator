0933cb14b5809f26ff847ba1490e30ba1da8de36
Daniel Lawrence <dan.lawrence0810@gmail.com>
2022-02-15 17:47:26 +1030

    tests started for intern class

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,74 +1,70 @@
 const Employee = require('../lib/Employee');
 
 describe('Employee', () => {
-  describe('Initialization', () => {
-    it('Should create an object with name, id and email if given valid arguments', () => {
-      const employee = new Employee('Dan', 1, 'dan@test.com');
+  it('Should create an object with name, id and email if given valid arguments', () => {
+    const employee = new Employee('Dan', 1, 'dan@test.com');
 
-      expect(employee.name).toEqual('Dan');
-      expect(employee.id).toEqual(1);
-      expect(employee.email).toEqual('dan@test.com');
-    });
+    expect(employee.name).toEqual('Dan');
+    expect(employee.id).toEqual(1);
+    expect(employee.email).toEqual('dan@test.com');
+  });
 
-    it('should throw an error if provided no arguments', () => {
-      const cb = () => new Employee();
+  it('should throw an error if provided no arguments', () => {
+    const cb = () => new Employee();
 
-      expect(cb).toThrow();
-    });
+    expect(cb).toThrow();
+  });
 
-    it('should throw an error if not provided an ID or email', () => {
-      const cb = () => new Employee('Dan');
+  it('should throw an error if not provided an ID or email', () => {
+    const cb = () => new Employee('Dan');
 
-      expect(cb).toThrow();
-    });
+    expect(cb).toThrow();
+  });
 
-    it('should throw an error if not provided an email', () => {
-      const cb = () => new Employee('Dan', 1);
+  it('should throw an error if not provided an email', () => {
+    const cb = () => new Employee('Dan', 1);
 
-      expect(cb).toThrow();
-    });
+    expect(cb).toThrow();
+  });
 
-    it("should throw an error if 'name' is not a string", () => {
-      const cb = () => new Employee(3, 2, 'dan@test.com');
-      const err = new Error(
-        "Expected parameter 'name' to be a non-empty string"
-      );
+  it("should throw an error if 'name' is not a string", () => {
+    const cb = () => new Employee(3, 2, 'dan@test.com');
+    const err = new Error("Expected parameter 'name' to be a non-empty string");
 
-      expect(cb).toThrowError(err);
-    });
+    expect(cb).toThrowError(err);
+  });
 
-    it("should throw an error if 'email' is not a string", () => {
-      const cb = () => new Employee('Dan', 2, 4);
-      const err = new Error(
-        "Expected parameter 'email' to be a non-empty string"
-      );
+  it("should throw an error if 'email' is not a string", () => {
+    const cb = () => new Employee('Dan', 2, 4);
+    const err = new Error(
+      "Expected parameter 'email' to be a non-empty string"
+    );
 
-      expect(cb).toThrowError(err);
-    });
+    expect(cb).toThrowError(err);
+  });
 
-    it("should throw an error if 'ID' is not a number", () => {
-      const cb = () => new Employee('Dan', '2', 'dan@test.com');
-      const err = new Error(
-        "Expected parameter 'ID' to be a non-negative number"
-      );
+  it("should throw an error if 'ID' is not a number", () => {
+    const cb = () => new Employee('Dan', '2', 'dan@test.com');
+    const err = new Error(
+      "Expected parameter 'ID' to be a non-negative number"
+    );
 
-      expect(cb).toThrowError(err);
-    });
+    expect(cb).toThrowError(err);
+  });
 
-    it("should throw an error if 'ID' is less than 0", () => {
-      const cb = () => new Employee('Dan', -1, 'dan@test.com');
-      const err = new Error(
-        "Expected parameter 'ID' to be a non-negative number"
-      );
+  it("should throw an error if 'ID' is less than 0", () => {
+    const cb = () => new Employee('Dan', -1, 'dan@test.com');
+    const err = new Error(
+      "Expected parameter 'ID' to be a non-negative number"
+    );
 
-      expect(cb).toThrowError(err);
-    });
+    expect(cb).toThrowError(err);
+  });
 
-    it("should throw an error if 'Email' does not contain @", () => {
-      const cb = () => new Employee('Dan', 1, 'hello');
-      const err = new Error('This must be a proper email address');
+  it("should throw an error if 'Email' does not contain @", () => {
+    const cb = () => new Employee('Dan', 1, 'hello');
+    const err = new Error('This must be a proper email address');
 
-      expect(cb).toThrowError(err);
-    });
+    expect(cb).toThrowError(err);
   });
 });
--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -1,2 +1,13 @@
 const Employee = require('../lib/Employee');
 const Intern = require('../lib/Intern');
+
+describe('Intern', () => {
+  it('should throw an error if not provided a school', () => {
+    const cb = () => new Intern('Dan', 1, 'dan@test.com');
+    const err = new Error(
+      "Expected parameter 'School' to be a non-empty string"
+    );
+
+    expect(cb).toThrowError(err);
+  });
+});
