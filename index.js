478cd1f8dac197f2ef192cc6f0ea8707fe12dc24
Daniel Lawrence <dan.lawrence0810@gmail.com>
2022-02-17 09:34:43 +1030

    added getOfficeNumber function and tried to add template html

--- index.js
+++ index.js
@@ -5,14 +5,115 @@ const Intern = require('./lib/Intern');
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const managerArray = [];
-const engineerArray = [];
-const internArray = [];
-let members;
+// const managerArray = [];
+// const engineerArray = [];
+// const internArray = [];
+const members = [];
 
 function writeToFile(fileName, data) {
   return fs.writeFileSync(fileName, JSON.stringify(data, null, 2));
 }
+
+const generateHTML = () =>
+  `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Team Profiles</title>
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
+      integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="style.css" />
+  </head>
+
+  <body>
+    <!-- when html is generated there should be a header saying team name followed 
+        by cards below with individual information for each team member -->
+
+    <header class="jumbotron jumbotron-fluid bg-warning">
+      <div class="container">
+        <h1 class="display-3 text-center">Team Name</h1>
+      </div>
+    </header>
+
+    <div class="d-flex justify-content-center">
+      <div class="card-deck">
+        <div id="manager">
+          <div class="card">
+            <div class="card-body">
+              <h4 class="card-title bg-primary">
+                ${manager.getName()}<br />${manager.getRole()}
+              </h4>
+              <ul class="list-group">
+                <li class="list-group-item">${manager.getID()}</li>
+                <li class="list-group-item">
+                  <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a>
+                </li>
+                <li class="list-group-item">${manager.getOfficeNumber()}</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div id="engineer">
+          <div class="card">
+            <div class="card-body">
+              <h4 class="card-title bg-primary">
+                ${engineer.getName}<br />${engineer.getRole()}
+              </h4>
+              <ul class="list-group">
+                <li class="list-group-item">${engineer.getID()}</li>
+                <li class="list-group-item">
+                  <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a>
+                </li>
+                <li class="list-group-item">
+                  <a href="https://github.com/${engineer.getGithub()}">${engineer.getGithub()}</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div id="intern">
+          <div class="card">
+            <div class="card-body">
+              <h4 class="card-title bg-primary">
+                ${intern.getName()}<br />${intern.getRole()}
+              </h4>
+              <ul class="list-group">
+                <li class="list-group-item">${intern.getID()}</li>
+                <li class="list-group-item">
+                  <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a>
+                </li>
+                <li class="list-group-item">${intern.getSchool()}</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <script
+      src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+      integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+      integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js"
+      integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
+`;
 
 const basicQuestions = [
   {
@@ -115,14 +216,14 @@ function askEngineerQuestions() {
   inquirer.prompt(engineerQuestions).then((data) => {
     const engineer = new Engineer(data.name, data.id, data.email, data.github);
     console.log(engineer.getRole());
-    engineerArray.push(engineer);
-    members = [...managerArray, ...engineerArray, ...internArray];
+    members.push(engineer);
+    // members = [...managerArray, ...engineerArray, ...internArray];
     if (data.add == 'Engineer') {
       askEngineerQuestions();
     } else if (data.add == 'Intern') {
       askInternQuestions();
     } else if (data.add == 'Finish') {
-      writeToFile('test.md', members);
+      writeToFile('index.html', generateHTML());
       console.log(members);
     }
   });
@@ -180,14 +281,14 @@ function askInternQuestions() {
   inquirer.prompt(internQuestions).then((data) => {
     const intern = new Intern(data.name, data.id, data.email, data.school);
     console.log(intern.getRole());
-    internArray.push(intern);
-    members = [...managerArray, ...engineerArray, ...internArray];
+    members.push(intern);
+    // members = [...managerArray, ...engineerArray, ...internArray];
     if (data.add == 'Engineer') {
       askEngineerQuestions();
     } else if (data.add == 'Intern') {
       askInternQuestions();
     } else if (data.add == 'Finish') {
-      writeToFile('test.md', members);
+      writeToFile('index.html', generateHTML());
       console.log(members);
     }
   });
@@ -203,14 +304,14 @@ inquirer
       data.officeNumber
     );
     console.log(manager.getRole());
-    managerArray.push(manager);
-    members = [...managerArray, ...engineerArray, ...internArray];
+    members.push(manager);
+    // members = [...managerArray, ...engineerArray, ...internArray];
     if (data.add == 'Engineer') {
       askEngineerQuestions();
     } else if (data.add == 'Intern') {
       askInternQuestions();
     } else if (data.add == 'Finish') {
-      writeToFile('test.md', members);
+      writeToFile('index.html', generateHTML());
       console.log(members);
     }
   })
